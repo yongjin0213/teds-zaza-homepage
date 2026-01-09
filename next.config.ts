a22592a6bdf925271c6728809f8ecff31388af21
Yongjin Lee <yongjin.lee.1108@gmail.com>
2026-01-09 09:23:55 +0100

    Updated CSP to allow google ads

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,46 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.tiktok.com https://*.tiktokcdn.com https://accounts.google.com https://pagead2.googlesyndication.com https://*.googleadservices.com https://*.google.com https://googleads.g.doubleclick.net https://www.googletagmanager.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+      https://www.tiktok.com https://*.tiktokcdn.com 
+      https://accounts.google.com 
+      https://pagead2.googlesyndication.com 
+      https://*.googleadservices.com 
+      https://*.google.com 
+      https://googleads.g.doubleclick.net 
+      https://*.adtrafficquality.google
+      https://www.googletagmanager.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://*.tiktok.com https://*.tiktokcdn.com https://teds-zaza-website.s3.amazonaws.com https://teds-zaza-website.s3.us-east-1.amazonaws.com https://*.googleadservices.com https://*.google.com https://googleads.g.doubleclick.net https://*.googlesyndication.com;
+    img-src 'self' blob: data: 
+      https://*.tiktok.com 
+      https://*.tiktokcdn.com 
+      https://teds-zaza-website.s3.amazonaws.com 
+      https://teds-zaza-website.s3.us-east-1.amazonaws.com 
+      https://*.googleadservices.com https://*.google.com 
+      https://googleads.g.doubleclick.net 
+      https://*.adtrafficquality.google
+      https://*.googlesyndication.com;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self' https://accounts.google.com;
-    frame-src 'self' https://www.tiktok.com https://accounts.google.com https://googleads.g.doubleclick.net https://*.googlesyndication.com https://tpc.googlesyndication.com;
-    connect-src 'self' https://teds-zaza-website.s3.amazonaws.com https://teds-zaza-website.s3.us-east-1.amazonaws.com https://accounts.google.com https://*.google-analytics.com https://*.googleadservices.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net;
+    frame-src 'self'
+      https://www.tiktok.com 
+      https://accounts.google.com 
+      https://googleads.g.doubleclick.net 
+      https://*.googlesyndication.com 
+      https://*.adtrafficquality.google
+      https://tpc.googlesyndication.com;
+    connect-src 'self' 
+      https://teds-zaza-website.s3.amazonaws.com 
+      https://teds-zaza-website.s3.us-east-1.amazonaws.com 
+      https://accounts.google.com https://*.google-analytics.com 
+      https://*.googleadservices.com 
+      https://pagead2.googlesyndication.com 
+      https://googleads.g.doubleclick.net
+      https://fundingchoicesmessages.google.com 
+      https://*.googlesyndication.com
+      https://*.adtrafficquality.google;
     frame-ancestors 'none';
 `.replace(/\n/g, "");
 
